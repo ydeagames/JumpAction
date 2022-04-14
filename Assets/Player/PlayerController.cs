01aff5788c20e4928efd0d6a49828768ebe647cd
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-04-14 09:32:39 +0900

    キャラクターの制御方法を変更
    横移動は AddForce ではなく velocity を使用。
    また空中でジャンプできる問題を解消。
    参考記事↓
    https://qiita.com/tokoroten_346/items/9a148e2ef84b7d1b0fef

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [Tooltip("ジャンプの強さ")]
     public float jumpForce = 1;
     private Rigidbody2D _rigidbody;
+    [SerializeField] LayerMask stageLayer;
+    [SerializeField] float endLinecastRatio = 0.3f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +23,37 @@ public class PlayerController : MonoBehaviour
     {
         // 入力をもらってリジッドに力を加える
         var xMove = Input.GetAxis("Horizontal");
-        _rigidbody.AddForce(xMove * Vector2.right * moveForce * Time.deltaTime);
+        var velocityX = xMove * Vector2.right * moveForce * Time.deltaTime;
+        _rigidbody.velocity = new Vector2(velocityX.x, _rigidbody.velocity.y);
 
         // ジャンプ
         if (Input.GetButtonDown("Jump"))
         {
-            _rigidbody.AddForce(jumpForce * Vector2.up * Time.deltaTime);
+            // 地面と接触しているかどうかチェック
+            if (GroundChk())
+            {
+                Debug.Log("Jump");
+                Jump();
+
+            }
         }
+    }
+
+    void Jump()
+    {
+        _rigidbody.AddForce(jumpForce * Vector2.up * Time.deltaTime);
+    }
+
+    // キャラクターがステージと接触しているかどうかを判定する
+    bool GroundChk()
+    {
+        Vector3 startposition = transform.position;                     // Playerの中心を始点とする
+        Vector3 endposition = transform.position - transform.up * endLinecastRatio; // Playerの足元を終点とする
+
+        // Debug用に始点と終点を表示する
+        Debug.DrawLine(startposition, endposition, Color.red);
+
+        // Physics2D.Linecastを使い、ベクトルとStageLayerが接触していたらTrueを返す
+        return Physics2D.Linecast(startposition, endposition, stageLayer);
     }
 }
